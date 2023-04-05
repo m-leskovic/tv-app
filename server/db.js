4a5e165e4d8d4a231d5514fa80c309dd49222943
Martina Leskovic <martina.leskovic@outlook.com>
2023-04-05 17:26:35 +0200

    Initial commit

--- server/db.js
+++ server/db.js
@@ -2,8 +2,7 @@ const { MongoClient } = require("mongodb");
 
 let dbName
 
-/* Replace <username> and <password> with your own MongoDB information */
-let url = "mongodb+srv://<username>:<password>@cluster0.x4nyzl2.mongodb.net/?retryWrites=true&w=majority"
+let url = "mongodb+srv://guest_user:uwp7fkbv6BSdb35@cluster0.x4nyzl2.mongodb.net/?retryWrites=true&w=majority"
 
 module.exports = {
     dbConnect: async (cb) => {
